ba137ab7adc84dd86854ba37b57364ccfc1d3676
H-IQ <48011944+H-IQ@users.noreply.github.com>
2020-08-28 13:13:45 +0200

    Update test.py

--- main/test.py
+++ main/test.py
@@ -76,8 +76,9 @@ for i in range(len(path)):
         outputs = fake_B
 
     filename = os.path.basename(path[i])
-    img_name = '/content/drive/My Drive/MBLLEN enhanced images/'
+    img_name = result_folder+'/' + filename
     # scipy.misc.toimage(outputs * 255, high=255, low=0, cmin=0, cmax=255).save(img_name)
     outputs = np.minimum(outputs, 1.0)
     outputs = np.maximum(outputs, 0.0)
     utls.imwrite(img_name, outputs)
+    save_images(os.path.join('/content/drive/My Drive/MBLLEN enhanced images/', '%s_kindle.png' % (img_name)), outputs)
